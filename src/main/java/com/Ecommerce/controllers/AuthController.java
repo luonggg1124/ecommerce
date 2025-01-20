e614afaa0a31e159799903e15b2f4fefa9157f5b
luonggg1124 <luongnm1124@gmail.com>
2025-01-20 14:59:21 +0700

    fix

---
+++ src/main/java/com/Ecommerce/controllers/AuthController.java
@@ -0,0 +1,12 @@
+package com.Ecommerce.controllers;
+
+import org.springframework.stereotype.Controller;
+
+
+
+public class AuthController {
+//    @GetMapping("/")
+//    public String home(){
+//        return "index";
+//    }
+}
